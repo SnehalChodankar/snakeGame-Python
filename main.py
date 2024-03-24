7c0aafaa1501cc34cd45ae5c60801200a541f45e
SnehalChodankar <snehalchodankar95@gmail.com>
2024-03-24 15:28:19 +0530

    initial snake setup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle, Screen
 
 screen = Screen()
@@ -5,20 +6,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+colors = ["green", "white", "orange", "yellow", "blue", "red"]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+for pos in starting_pos:
+    new_seg = Turtle("square")
+    new_seg.penup()
+    new_seg.color(random.choice(colors))
+    new_seg.setpos(pos)
 
 
 
